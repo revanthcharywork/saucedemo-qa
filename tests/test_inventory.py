025f4ad2b6fd68aee78a15f61eab26433aca3779
Revanth Chary Tukkapuram <revanth00744@gmail.com>
2026-06-10 15:26:16 -0500

    Fix: replace 'is' with '==' in test_inventory_item_count

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -10,7 +10,7 @@ def test_inventory_page_loads(logged_in_page):
 @pytest.mark.inventory
 def test_inventory_item_count(logged_in_page):
     inv = Inventory(logged_in_page)
-    assert inv.items_count() is 6 , f"Inventory items count expected to be 6, But got {inv.items_count()}"
+    assert inv.items_count() == 6 , f"Inventory items count expected to be 6, But got {inv.items_count()}"
 
 @pytest.mark.inventory
 def  test_sort_products_low_to_high(logged_in_page):
@@ -23,6 +23,16 @@ def  test_sort_products_low_to_high(logged_in_page):
     assert prices == sorted(prices), "Prices not sorted low to high"
 
 @pytest.mark.inventory
+def test_sort_products_high_to_low(logged_in_page):
+    inv = Inventory(logged_in_page)
+    inv.sort_items("hilo")
+    items_data = inv.get_item_details()
+    prices = []
+    for item in items_data:
+        prices.append(item[1])
+    assert prices == sorted(prices, reverse=True), "Prices not sorted high to low"
+
+@pytest.mark.inventory
 def test_sort_products_a_to_z(logged_in_page):
     inv = Inventory(logged_in_page)
     inv.sort_items("az")
@@ -31,4 +41,14 @@ def test_sort_products_a_to_z(logged_in_page):
     for item in items_data:
         product_names.append(item[0])
     assert product_names == sorted(product_names), "Product names not sorted low to high"
+
+@pytest.mark.inventory
+def test_sort_products_z_to_a(logged_in_page):
+    inv = Inventory(logged_in_page)
+    inv.sort_items("za")
+    items_data = inv.get_item_details()
+    product_names = []
+    for item in items_data:
+        product_names.append(item[0])
+    assert product_names == sorted(product_names, reverse=True), "Product names not sorted high to low"
 
